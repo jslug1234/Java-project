4ef21ee47261cc0667ace5b4efe63e8903d23d9e
jl2191@trevor.org <joshlovins@Joshs-MacBook-Air.local>
2012-09-11 13:03:19 -0400

    hello program

---
+++ Hello/src/hello/HelloClass.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class HelloClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("First");
+		
+	} //end of main
+
+} //end of HelloClass
